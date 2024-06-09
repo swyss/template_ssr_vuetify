b2a25a4fd46b7a3bc2a98a5fc50f40fa44358eb0
s.wyss <stefan.arnold.wyss@gmail.com>
2024-06-09 21:39:28 +0200

    add new project

---
+++ frontend/vuetify-project/src/layouts/default.vue
@@ -0,0 +1,57 @@
+<template>
+  <v-app>
+    <v-app-bar :elevation="13">
+      <template v-slot:prepend>
+        <v-app-bar-nav-icon></v-app-bar-nav-icon>
+      </template>
+
+      <v-app-bar-title>Application Bar</v-app-bar-title>
+
+      <template v-slot:append>
+        <v-btn icon="mdi-heart"></v-btn>
+
+        <v-btn icon="mdi-magnify"></v-btn>
+
+        <v-btn icon="mdi-dots-vertical"></v-btn>
+      </template>
+    </v-app-bar>
+
+    <v-navigation-drawer>
+      <v-list>
+        <v-list-item title="Navigation drawer"></v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <v-main>
+      <router-view />
+    </v-main>
+    <v-bottom-navigation
+      v-model="value"
+      color="primary"
+      active
+    >
+      <v-btn>
+        <v-icon>mdi-history</v-icon>
+
+        Recents
+      </v-btn>
+
+      <v-btn>
+        <v-icon>mdi-heart</v-icon>
+
+        Favorites
+      </v-btn>
+
+      <v-btn>
+        <v-icon>mdi-map-marker</v-icon>
+
+        <span>Nearby</span>
+      </v-btn>
+    </v-bottom-navigation>
+    <AppFooter />
+  </v-app>
+</template>
+
+<script setup>
+  //
+import AppFooter from "@/components/AppFooter.vue";
+</script>
